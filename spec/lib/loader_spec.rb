c119fdfc111399714f469bed152719b9f4424446
Josh Nesbitt <josh@josh-nesbitt.net>
2010-09-29 11:50:29 +0100

    Fixed failing loader specs.

--- spec/lib/loader_spec.rb
+++ spec/lib/loader_spec.rb
@@ -3,35 +3,28 @@ module Project
     
     before do
       @config_path = File.join(ROOT, 'spec', 'fixtures', 'config.yml')
-      Loader.config_path(@config_path)
-    end
-    
-    it "should allow a config path to be set" do
-      path = "/my/path/.project"
-      Loader.config_path(path)
-      Loader.config_path.should == path
+      @loader = Loader.new(@config_path)
     end
     
     it "should create a configuration file on #load! if one does not exist" do
       FileUtils.rm(@config_path) if File.exists?(@config_path)
       File.exists?(@config_path).should == false
       
-      loader = Loader.new
-      lambda { loader.load! }.should raise_error(SystemExit)
+      lambda { @loader.load! }.should raise_error(SystemExit)
       
       File.exists?(@config_path).should == true
     end
     
     it "should load Project and Workflow configurations if a configuration exists" do
-      loader = Loader.new
-      
       unless File.exists?(@config_path)
-        lambda { loader.load! }.should raise_error(SystemExit)
+        lambda { @loader.load! }.should raise_error(SystemExit)
         File.exists?(@config_path).should == true
       end
       
-      loader.load!
-      loader.raw_config.should_not be_nil
+      @loader.load!
+      @loader.raw_config.class.should == Hash
+      @loader.raw_config[:projects].include?(:example).should == true
+      @loader.raw_config[:workflows].include?(:default).should == true
     end
   end
 end
